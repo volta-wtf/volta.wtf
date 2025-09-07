a59445492e84e283f8dde859cae3c984b47cfd5e
Cristian <cristianferrarig@gmail.com>
2025-09-07 12:15:23 -0300

    Ready to deploy

--- packages/ui/eslint.config.mjs
+++ packages/ui/eslint.config.mjs
@@ -1,4 +1,4 @@
-import { config } from "@repo/eslint-config/react-internal";
+import { config } from "@repo/config-eslint/react-internal";
 
 /** @type {import("eslint").Linter.Config} */
 export default config;
