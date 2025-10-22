b6dccdc16ec656f44f242bc5092f0270a752108c
Cristian <cristianferrarig@gmail.com>
2025-10-22 13:48:55 -0300

    Again.

--- apps/catalog/next.config.js
+++ apps/catalog/next.config.js
@@ -3,6 +3,21 @@ const nextConfig = {
     experimental: {
         optimizePackageImports: ['react', 'react-dom', 'next'],
     },
+    outputFileTracingExcludes: {
+        "/*": [
+            "node_modules/@swc/core-linux-x64-gnu",
+            "node_modules/@swc/core-linux-x64-musl",
+            "node_modules/@esbuild/darwin-arm64",
+            "node_modules/@esbuild/linux-x64",
+            "node_modules/webpack",
+            "node_modules/terser",
+            "../../**/node_modules",
+            "../../apps/*/node_modules",
+            "../../packages/*/node_modules",
+            "../../registry/*/node_modules",
+            "../../sites/*/node_modules",
+        ],
+    },
     transpilePackages: [
         '@registry/styles',
         '@registry/primitives',
